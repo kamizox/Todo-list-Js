268255c6d15741893269576a3640810bc3dc7c3d
Kamran Alam <Kamrankhan0368@gmail.com>
2025-08-19 00:34:51 +0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+var Todos = [];
+
+function addTodo(){
+
+    var inputElement = document.getElementById("inputTask")
+    Todos.push(inputElement.value);
+    // inputElement.value = "";
+}
+
+function showlist()
+{
+    var listItems = document.getElementById("todo-list")
+    
+    
+}
