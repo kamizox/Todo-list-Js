c24cffb19622ee8fc70fc7202c1d427154e0d112
Kamran Alam <Kamrankhan0368@gmail.com>
2025-08-20 14:31:26 +0500

    updated

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ function addTodo(){
 
     var inputElement = document.getElementById("inputTask")
     Todos.push(inputElement.value);
-    // inputElement.value = "";
+    inputElement.value = "";
 }
 
 function showlist()
 {
     var listItems = document.getElementById("todo-list")
+    for(var i=0; i<Todos.length; i++)
+    {
+        listItems.innerHTML = '<li> html </li>'
+    }
     
     
 }
