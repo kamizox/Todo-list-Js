154cb9457d739d3fba3c4396f6ae0502455b7307
Kamran Alam <Kamrankhan0368@gmail.com>
2025-08-20 23:57:03 +0500

    updated

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 
 var Todos = [];
-
+var editValue = -1;
 function addTodo(){
 
     var inputElement = document.getElementById("inputTask")
-    Todos.push(inputElement.value);
+    var value = inputElement.value.trim();
     inputElement.value = "";
-}
+    if(value === ""){return}
 
+    if(editValue == -1)
+    {
+        Todos.push(value);
+    }else
+    {
+        Todos[editInd] = value;
+        editInd = -1;
+    }
+    inputElement.value = "";
+    showlist();
+}
 function showlist()
 {
     var listItems = document.getElementById("todo-list")
     listItems.innerHTML = "";
     for(var i=0; i<Todos.length; i++)
     {
-        listItems.innerHTML += `<li> ${Todos[i]} <button onclick="delet(${i})">delet</button> </li>`
+        listItems.innerHTML += `<li> ${Todos[i]}
+        <button onclick="update(${i})">Edit</button>  
+        <button onclick="delet(${i})">delet</button> </li>`
     } 
 }
 showlist();
-
 function delet(delitm)
 {
      Todos.splice(delitm,1);
      showlist();
+}
+
+
+function updadt(ind)
+{
+    var inputElement = document.getElementById("inputask");
+    inputElement.value = Todos[ind];
+
 }
